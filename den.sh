d7e62bec53421a1ff60a654a8f14c7d1cfcd944c
Jared R. Kannianen <98085138+JarKanni@users.noreply.github.com>
2024-08-22 16:34:01 -0500

    scripts to ssh into remote servers

---
+++ den.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+echo -e '\nDown to the Den...\n'
+
+ssh peachy@192.168.1.24
---
+++ forge.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+echo -e '\nEntering the Lab, sir...\n'
+
+ssh peachy@192.168.1.18
